8e403946b0c413cc840efd454a094b19efc28d8a
Page <sarah.page@dealerdirect.com>
2023-08-08 23:18:39 +0200

    Adds some static initial scenery and a player

--- main.js
+++ main.js
@@ -5,22 +5,27 @@ const gameWidth = window.innerWidth;
 const gameHeight = Utils.floor(gameWidth * 0.5636160714285714);
 
 const farBackgroundCanvas = document.createElement("canvas");
+farBackgroundCanvas.id = "farBackgroundCanvas";
 farBackgroundCanvas.width = gameWidth;
 farBackgroundCanvas.height = gameHeight;
 
 const backgroundCanvas = document.createElement("canvas");
+backgroundCanvas.id = "backgroundCanvas";
 backgroundCanvas.width = gameWidth;
 backgroundCanvas.height = gameHeight;
 
 const wallsCanvas = document.createElement("canvas");
+wallsCanvas.id = "wallsCanvas";
 wallsCanvas.width = gameWidth;
 wallsCanvas.height = gameHeight;
 
 const mainCanvas = document.createElement("canvas");
+mainCanvas.id = "mainCanvas";
 mainCanvas.width = gameWidth;
 mainCanvas.height = gameHeight;
 
 const foregroundCanvas = document.createElement("canvas");
+foregroundCanvas.id = "foregroundCanvas";
 foregroundCanvas.width = gameWidth;
 foregroundCanvas.height = gameHeight;
 
@@ -33,4 +38,23 @@ content.appendChild(foregroundCanvas);
 
 document.body.appendChild(content);
 
-const canvasRenderer = new CanvasRenderer(mainCanvas);
+const farBackgroundCanvasRenderer = new CanvasRenderer(farBackgroundCanvas);
+const backgroundCanvasRenderer = new CanvasRenderer(backgroundCanvas);
+const wallsCanvasRenderer = new CanvasRenderer(wallsCanvas);
+const mainCanvasRenderer = new CanvasRenderer(mainCanvas);
+const foregroundCanvasRenderer = new CanvasRenderer(foregroundCanvas);
+
+farBackgroundCanvasRenderer.fill("#888");
+
+backgroundCanvasRenderer.drawRect(0, 0, gameWidth, 150, "#555");
+
+wallsCanvasRenderer.drawRect(100, 0, 15, 300, "#222");
+wallsCanvasRenderer.drawRect(480, 0, 8, 300, "#222");
+wallsCanvasRenderer.drawRect(860, 0, 15, 300, "#222");
+
+mainCanvasRenderer.drawRect(0, 0, gameWidth, 70, "#111");
+mainCanvasRenderer.drawRect(300, 70, 30, 100, "#933");
+
+foregroundCanvasRenderer.drawRect(-20, 0, 45, gameHeight, "#000")
+foregroundCanvasRenderer.drawRect(400, 0, 20, gameHeight, "#000");
+foregroundCanvasRenderer.drawRect(890, 0, 45, gameHeight, "#000");
