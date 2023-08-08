5bb9c42f53ae0c10e316020c9e43d326ff2a3c85
Page <sarah.page@dealerdirect.com>
2023-08-08 20:08:58 +0200

    Initial boilerplating

---
+++ testBot/testBot.js
@@ -0,0 +1,205 @@
+import { Utils } from "../utils.js";
+import { DivResultsRenderer } from "./resultRenderers/DivResultsRenderer.js";
+
+class Suite {
+  name;
+
+  tests = [];
+
+  constructor(suiteName) {
+    this.name = suiteName;
+  }
+
+  addTest(testName, testMethod) {
+    this.tests.push({
+      name: testName,
+      fnTest: testMethod,
+    });
+  }
+}
+
+export class TestBot {
+  static TEST_SUCCEEDED = 0;
+  static TEST_FAILED = 1;
+  static TEST_ERROR = 2;
+  static TEST_MISSING = 3;
+
+  testSuites = [];
+
+  expected;
+
+  expectedError;
+
+  actual;
+
+  result;
+
+  constructor(resultRenderer) {
+    this.resultRenderer = resultRenderer;
+  }
+
+  static renderResultsInDiv(container) {
+    return new DivResultsRenderer(container);
+  }
+
+  createSuite(testSuiteName) {
+    const newSuite = new Suite(testSuiteName);
+    this.testSuites.push(newSuite);
+
+    return newSuite;
+  }
+
+  runSuite(suite) {
+    const tests = suite.tests;
+    tests.forEach((test) => {
+      this.expected = undefined;
+      this.actual = undefined;
+      this.expectedError = undefined;
+      this.result = TestBot.TEST_MISSING;
+      let caughtError = undefined;
+
+      try {
+        test.fnTest();
+      } catch (error) {
+        caughtError = error;
+      }
+
+      if (Utils.isDefined(this.expectedError) && Utils.isDefined(caughtError)) {
+        if (Utils.isInstanceOf(this.expectedError, caughtError)) {
+          this.result = TestBot.TEST_SUCCEEDED;
+        } else {
+          this.result = TestBot.TEST_FAILED;
+          this.expected = this.expectedError.prototype.name;
+          this.actual = caughtError.name;
+        }
+      }
+
+      if (
+        Utils.isUndefined(this.expectedError) &&
+        Utils.isDefined(caughtError)
+      ) {
+        this.result = TestBot.TEST_ERROR;
+      }
+
+      if (
+        Utils.isDefined(this.expectedError) &&
+        Utils.isUndefined(caughtError)
+      ) {
+        this.expected = this.expectedError.prototype.name;
+        this.result = TestBot.TEST_FAILED;
+      }
+
+      if (TestBot.TEST_ERROR === this.result) {
+        console.log(`"${test.name}" caused the following unexpected error:`);
+        console.error(caughtError);
+      }
+
+      if (TestBot.TEST_FAILED === this.result) {
+        console.log(
+          `"${test.name}" failed:\n\tExpected:\n\t\t`,
+          this.expected,
+          "\n\tActual:\n\t\t",
+          this.actual
+        );
+      }
+
+      this.resultRenderer.addResult(
+        suite.name,
+        test.name,
+        this.result,
+        this.expected,
+        this.actual
+      );
+    });
+  }
+
+  run() {
+    this.testSuites.forEach((suite) => {
+      this.runSuite(suite);
+    });
+  }
+
+  assertStrictlyEquals(expected, actual) {
+    this.expected = expected;
+    this.actual = actual;
+    this.result =
+      expected === actual ? TestBot.TEST_SUCCEEDED : TestBot.TEST_FAILED;
+  }
+
+  assertThrowsExpectedError(expectedError) {
+    this.expectedError = expectedError;
+  }
+
+  assertDeepCompareObjects(expected, actual) {
+    this.expected = expected;
+    this.actual = actual;
+    this.result = Utils.objectEquals(expected, actual)
+      ? TestBot.TEST_SUCCEEDED
+      : TestBot.TEST_FAILED;
+  }
+
+  assertInRange(min, actual, max) {
+    if (!Utils.isNumber(actual)) {
+      throw new TypeError(
+        `Provided 'actual' value must be of type number, '${actual}' given.`
+      );
+    }
+
+    if (!Utils.isNumber(min)) {
+      throw new TypeError(
+        `Minimum range value must be of type number, '${min}' given.`
+      );
+    }
+
+    if (!Utils.isNumber(max)) {
+      throw new TypeError(
+        `Maximum range value must be of type number, '${max}' given.`
+      );
+    }
+
+    this.expected = `[${min}, ${max}]`;
+    this.actual = actual;
+    this.result =
+      actual < min || max < actual
+        ? TestBot.TEST_FAILED
+        : TestBot.TEST_SUCCEEDED;
+  }
+
+  assertGreaterThan(expected, actual) {
+    if (!Utils.isNumber(expected)) {
+      throw new TypeError(
+        `Expected value must be number, '${typeof expected}' given.`
+      );
+    }
+
+    if (!Utils.isNumber(actual)) {
+      throw new TypeError(
+        `Actual value must be number, '${typeof actual}' given.`
+      );
+    }
+
+    this.expected = `> ${expected}`;
+    this.actual = actual;
+    this.result =
+      actual > expected ? TestBot.TEST_SUCCEEDED : TestBot.TEST_FAILED;
+  }
+
+  assertLessThan(expected, actual) {
+    if (!Utils.isNumber(expected)) {
+      throw new TypeError(
+        `Expected value must be number, '${typeof expected}' given.`
+      );
+    }
+
+    if (!Utils.isNumber(actual)) {
+      throw new TypeError(
+        `Actual value must be number, '${typeof actual}' given.`
+      );
+    }
+
+    this.expected = `< ${expected}`;
+    this.actual = actual;
+    this.result =
+      actual < expected ? TestBot.TEST_SUCCEEDED : TestBot.TEST_FAILED;
+  }
+}
---
+++ utils.js
@@ -0,0 +1,212 @@
+/* eslint
+    no-bitwise: ["error", { "allow": ["|", "^"] }]
+ */
+
+export class Utils {
+  static isUndefined(value) {
+    // NOTE 1: value being a reference to something.
+
+    /* NOTE 2: changed the use of: */
+    // return (value === undefined);
+    /* to: */
+    return typeof value === "undefined";
+    /* as this is supported on more browsers according to Teun Lassche */
+  }
+
+  static isDefined(value) {
+    return !Utils.isUndefined(value);
+  }
+
+  static isBoolean(value) {
+    return typeof value === "boolean";
+  }
+
+  static isNumber(num) {
+    return typeof num === "number";
+  }
+
+  static isInteger(value) {
+    return value === +value && value === (value | 0);
+  }
+
+  static isString(value) {
+    return typeof value === "string";
+  }
+
+  static isNull(value) {
+    return value === null;
+  }
+
+  static isObject(value) {
+    // #NOTE1: typeof null === 'object' so check for null as well!
+    if (Utils.isNull(value)) {
+      return false;
+    }
+    if (Utils.isUndefined(value)) {
+      return false;
+    }
+
+    return typeof value === "object";
+  }
+
+  static isFunction(value) {
+    return typeof value === "function";
+  }
+
+  static isEmptyObject(value) {
+    if (!Utils.isObject(value)) {
+      return false;
+    }
+
+    return Object.getOwnPropertyNames(value).length === 0;
+  }
+
+  static isArray(value) {
+    return Array.isArray(value);
+  }
+
+  static constrain(value, lowerBound, upperBound) {
+    return Math.max(lowerBound, Math.min(upperBound, value));
+  }
+
+  static objectEquals(x, y) {
+    if (
+      Utils.isNull(x) ||
+      Utils.isNull(y) ||
+      Utils.isUndefined(x) ||
+      Utils.isUndefined(y)
+    ) {
+      return x === y;
+    }
+
+    // after this just checking type of one would be enough
+    if (x.constructor !== y.constructor) {
+      return false;
+    }
+
+    // if they are functions, they should exactly refer to same one (because of closures)
+    // if they are regexps, they should exactly refer to same one
+    //    it is hard to better equality check on current ES
+    if (x instanceof Function || x instanceof RegExp) {
+      return x === y;
+    }
+
+    if (x === y || x.valueOf() === y.valueOf()) {
+      return true;
+    }
+
+    if (Utils.isArray(x) && x.length !== y.length) {
+      return false;
+    }
+
+    // if they are dates, they must've had equal valueOf
+    if (x instanceof Date) {
+      return false;
+    }
+
+    // if they are strictly equal, they both need to be object at least
+    if (!(Utils.isObject(x) && Utils.isObject(y))) {
+      return false;
+    }
+
+    // recursive object equality check
+    const xKeys = Object.keys(x);
+    const yKeysInX = Object.keys(y).every((i) => xKeys.indexOf(i) !== -1);
+    const valuesEqual = xKeys.every((i) => Utils.objectEquals(x[i], y[i]));
+
+    return yKeysInX && valuesEqual;
+  }
+
+  static shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = (Math.random() * (i + 1)) | 0;
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  static faultOnError(err) {
+    const errBody = document.createElement("body");
+    errBody.style.backgroundColor = "#cc3333";
+    errBody.style.color = "#ffffff";
+    errBody.innerHTML = `<pre>${err}</pre>`;
+    document.body = errBody;
+
+    throw err;
+  }
+
+  static attempt(toAttempt, ...args) {
+    let result;
+
+    try {
+      result = toAttempt(...args);
+    } catch (err) {
+      Utils.faultOnError(err);
+    }
+
+    return result;
+  }
+
+  static report(...args) {
+    // eslint-disable-next-line no-console
+    console.log(...args);
+  }
+
+  static reportError(error) {
+    throw new Error(error);
+  }
+
+  static onlyProceedIf(statement, check) {
+    if (Utils.attempt(check, [statement]) !== true) {
+      Utils.reportError("A checkpoint failed, check the stack for more info.");
+    }
+  }
+
+  static getTime() {
+    return Date.now();
+  }
+
+  static generateRandomFloat(pMax = 1.0, pMin = 0.0) {
+    if (!Utils.isNumber(pMax)) {
+      throw new TypeError(`Provided max should be a number, '${pMax}' given.`);
+    }
+
+    if (!Utils.isNumber(pMin)) {
+      throw new TypeError(`Provided min should be a number, '${pMin}' given.`);
+    }
+
+    if (pMin > pMax) {
+      throw new TypeError(
+        `Provided max value needs to be greater than min value`
+      );
+    }
+
+    return Math.random() * (pMax - pMin) + pMin;
+  }
+
+  static generateRandomNumber(pMax, pMin) {
+    let max = pMax;
+    let min = pMin;
+
+    if (!Utils.isInteger(max)) {
+      max = 100;
+    }
+    if (!Utils.isInteger(min)) {
+      min = 0;
+    }
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+
+    return randomNumber;
+  }
+
+  static reportUsageError(error) {
+    // For now just report the error normally;
+    Utils.reportError(error);
+  }
+
+  static isInstanceOf(parentClass, childClass) {
+    return Object.prototype.isPrototypeOf.call(
+      parentClass.prototype,
+      childClass
+    );
+  }
+}
