321d4dc90839ed4dd8153d23a499d464ab10b3f2
Page <sarah.page@dealerdirect.com>
2023-08-09 22:05:46 +0200

    Adds hard error capture to mainloop

--- components/mainloop.js
+++ components/mainloop.js
@@ -10,13 +10,23 @@ import { Utils } from "../utils.js";
   currently this works and the complexity of this psuedo-class is minimal enough to not merrit
   the time investment.
  */
-export function createMainloop(frameRender) {
-  if (!Utils.isFunction(frameRender)) {
+export function createMainloop(fnRender) {
+  if (!Utils.isFunction(fnRender)) {
     Utils.reportUsageError("Usage: createMainloop(frameRender: function");
   }
 
   let isAnimating = false;
   let isDebugging = false;
+
+  function frameRender() {
+    try {
+      fnRender();
+    } catch (error) {
+      stop();
+
+      Utils.faultOnError(error);
+    }
+  }
 
   function tic() {
     if (isAnimating === true) {
@@ -25,37 +35,49 @@ export function createMainloop(frameRender) {
     }
   }
 
+  function start() {
+    if (isAnimating === true) {
+      return;
+    }
+
+    isAnimating = true;
+
+    if (isDebugging) {
+      Utils.report("Animation started");
+    }
+
+    tic();
+  }
+
+  function stop() {
+    isAnimating = false;
+    if (isDebugging) {
+      Utils.report("Animation stopped");
+    }
+  }
+
+  function reset() {
+    throw new SyntaxError("reset not yet implemented");
+  }
+
+  function next() {
+    if (isAnimating === true) {
+      return;
+    }
+
+    frameRender();
+  }
+
+  function setDebug(bVal) {
+    isDebugging = bVal;
+  }
+
   const protoMainloop = {
-    start: function start() {
-      if (isAnimating === true) {
-        return;
-      }
-
-      isAnimating = true;
-
-      if (isDebugging) {
-        Utils.report("Animation started");
-      }
-
-      tic();
-    },
-    stop: function stop() {
-      isAnimating = false;
-      if (isDebugging) {
-        Utils.report("Animation stopped");
-      }
-    },
-    reset: function reset() {},
-    next: function next() {
-      if (isAnimating === true) {
-        return;
-      }
-
-      frameRender();
-    },
-    setDebug: function setDebug(bVal) {
-      isDebugging = bVal;
-    },
+    start: start,
+    stop: stop,
+    reset: reset,
+    next: next,
+    setDebug: setDebug,
   };
 
   return protoMainloop;
