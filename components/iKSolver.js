01994f01178a2cb7da5fd6b1676754eaac0a601b
Page <sarah.page@dealerdirect.com>
2023-08-18 16:19:41 +0200

    Changes IKSolver class to have local and global methods

--- components/iKSolver.js
+++ components/iKSolver.js
@@ -6,19 +6,37 @@ import { Vector } from "./vector.js";
 //   console.log(x, v, v / Math.PI * 180);
 // }
 
-export function IKSolver(v, l1, l2, posEE, dirBend = 1) {
-  /**
-   *      / l1^2 + tx^2 + ty^2 - l2^2 \
-   * acos| --------------------------- |  =>  ±[0, π]
-   *      \  2 * l1 * √(tx^2 + ty^2)  /
-   */
-  const numerator = l1 * l1 + posEE.x * posEE.x + posEE.y * posEE.y - l2 * l2;
-  const denominator = 2 * l1 * posEE.length();
+export class IKSolver {
+  static #localPosEE = new Vector(0, 0);
 
-  let angleBendLocal = dirBend * Math.acos(numerator / denominator);
-  if (isNaN(angleBendLocal)) {
-    angleBendLocal = 0;
+  static local(v, l1, l2, posEE, dirBend = 1) {
+    /**
+     *      / l1^2 + tx^2 + ty^2 - l2^2 \
+     * acos| --------------------------- |  =>  ±[0, π]
+     *      \  2 * l1 * √(tx^2 + ty^2)  /
+     */
+    const numerator = l1 * l1 + posEE.x * posEE.x + posEE.y * posEE.y - l2 * l2;
+    const denominator = 2 * l1 * posEE.length();
+
+    let angleBendLocal = dirBend * Math.acos(numerator / denominator);
+    if (isNaN(angleBendLocal)) {
+      angleBendLocal = 0;
+    }
+
+    return Vector.setPolar(v, angleBendLocal + posEE.angle(), l1);
   }
 
-  return Vector.setPolar(v, angleBendLocal + posEE.angle(), l1);
+  static global(v, l1, l2, posEE, origin, dirBend = 1) {
+    // we need to translate the whole thing to (0, 0), so subtract 'origin' from everything
+    // but we only receive the end effector 'posEE'
+    // so we just need to subtract origin from the posEE vector
+    IKSolver.#localPosEE.copyFrom(posEE);
+    IKSolver.#localPosEE.subtract(origin);
+
+    // Now iksolve it locally:
+    IKSolver.local(v, l1, l2, IKSolver.#localPosEE, dirBend);
+
+    // But now we have v in local space, so add origin to that to place back in global space
+    v.add(origin);
+  }
 }
--- tests/iKSolverTests.js
+++ tests/iKSolverTests.js
@@ -11,8 +11,48 @@ const testRunner = new TestBot(resultRenderer);
 
 const iKSolverTests = testRunner.createSuite("Tests IKSolver");
 
-iKSolverTests.addTest("is a function", () => {
-  testRunner.assertStrictlyEquals('function', typeof IKSolver);
+iKSolverTests.addTest("has local solve function", () => {
+  testRunner.assertStrictlyEquals("function", typeof IKSolver.local);
+});
+
+iKSolverTests.addTest("has global solve function", () => {
+  testRunner.assertStrictlyEquals("function", typeof IKSolver.global);
+});
+
+iKSolverTests.addTest("calc knee from origin", () => {
+  // This is origin so (0, 0)
+  const hip = new Vector(0, 0);
+  // This is determined so initialise with (0, 0) we move it with IKSolver
+  const knee = new Vector(0, 0);
+  // This is the end effector, we position it at (8, 0)
+  const foot = new Vector(8, 0);
+
+  // Now if we put the length of both limbs at 5, then pythagorem should tell us knee should end
+  // up at exactly (4, 3);
+  IKSolver.local(knee, 5, 5, foot);
+  // knee = {x: 4, y: 2.999999999999999}...
+  // Ok, maybe not _exactly_... close enough!
+  const actual = knee;
+
+  const expected = new Vector(4, 3);
+
+  testRunner.assertStrictlyEquals(true, Vector.equal(expected, actual));
+});
+
+iKSolverTests.addTest("calc knee in global space", () => {
+  // Origin is now placed 100 to the right
+  const hip = new Vector(100, 8);
+  // Foot is now directly underneath hip, 100 to the right
+  const foot = new Vector(100, 0);
+  // Initialise knee vector;
+  const actual = new Vector(0, 0);
+
+  // Now iksolve it:
+  IKSolver.global(actual, 5, 5, foot, hip);
+
+  const expected = new Vector(103, 4);
+
+  testRunner.assertStrictlyEquals(true, Vector.equal(expected, actual));
 });
 
 testRunner.run();
