cc00d6a1e1a599dbd8790cd67ff1960b25fb9a13
Sarah Page <page.synid@gmail.com>
2024-12-16 14:29:55 +0100

    Changes swingTest.js cleaning up old stuff and adding animation blending

--- swingTest.js
+++ swingTest.js
@@ -11,100 +11,33 @@ import Renderer2d from "./components/renderer2d.js";
 import Line from "./components/line.js";
 import Rectangle from "./components/rectangle.js";
 import IKSolver from "./components/ikSolver.js";
-
-// /**
-//  * @param {Line} sword
-//  */
-// function updateSword(sword, elapsed) {
-//     // const animSpeed = 0.25;
-//     const animSpeed = 1;
-//     if (CheekyTicCounter > 1.3 * animSpeed) {
-//         CheekyTicCounter = 0;
-//     }
-
-//     CheekyTicCounter += elapsed;
-//     const animationTic = CheekyTicCounter / animSpeed;
-//     const newStart = new Vector(sword.start.x, sword.start.y);
-//     const newEnd = new Vector(sword.end.x, sword.end.y);
-
-//     const otherAnimX = Lerp.calc(-50, 50, Lerp.parabola(animationTic))
-//     const otherAnimY = Lerp.calc(0, -50, Lerp.linear(animationTic));
-//     const x1Offset = 0 + otherAnimX;
-//     const y1Offset = 0 + otherAnimY;
-//     const x2Offset = Lerp.calc(-150, 200, Lerp.parabola(animationTic)) + otherAnimX;
-//     const y2Offset = Lerp.calc(0, -250, Lerp.linear(animationTic)) + otherAnimY;
-
-//     newStart.x += x1Offset;
-//     newStart.y += y1Offset;
-//     newEnd.x += x2Offset;
-//     newEnd.y += y2Offset;
-
-//     return new Line(newStart, newEnd, sword.c, sword.lw);
-// }
-
-// function updateSword2(sword, elapsed) {
-//     let xStartOffset = 0;
-//     let yStartOffset = 0;
-//     let xEndOffset = 0;
-//     let yEndOffset = 0;
-
-//     for (const animation of animations) {
-//         animation.progress += elapsed;
-//         const tic = animation.progress % animation.duration / animation.duration;
-//         xStartOffset += animation.xStart(tic);
-//         yStartOffset += animation.yStart(tic);
-//         xEndOffset += animation.xEnd(tic);
-//         yEndOffset += animation.yEnd(tic);
-//     }
-
-//     return new Line(
-//         new Vector(sword.start.x + xStartOffset, sword.start.y + yStartOffset),
-//         new Vector(sword.end.x + xEndOffset, sword.end.y + yEndOffset),
-//         sword.c, sword.lw
-//     )
-// }
+import Utils from "./utils.js";
+import Drawable from "./components/drawable.js";
 
 /**
- * @param {Line} sword
+ * @param {*} bones
  * @param {Array} animations
  * @param {Number} progress
  */
-function updateSword3(sword, animations, progress) {
-    let xStartOffset = 0;
-    let yStartOffset = 0;
-    let xEndOffset = 0;
-    let yEndOffset = 0;
-
+function updateBones(original, bones, animations, progress, elapsed) {
     for (const animation of animations) {
-        xStartOffset += animation.xStart(progress);
-        yStartOffset += animation.yStart(progress);
-        xEndOffset += animation.xEnd(progress);
-        yEndOffset += animation.yEnd(progress);
+        for (const boneName in animation) {
+            for (const point in animation[boneName]) {
+                const newVal = original[boneName][point] + (animation[boneName][point](progress) * 4);
+                // bones[boneName][point] += animation[boneName][point](progress) * 4;
+                bones[boneName][point] = Lerp.expDecay(bones[boneName][point], newVal, 30, elapsed);
+            }
+        }
     }
-
-    sword.x1 += playerPosition.x + xStartOffset;
-    sword.y1 += playerPosition.y + yStartOffset;
-    sword.x2 += playerPosition.x + xEndOffset;
-    sword.y2 += playerPosition.y + yEndOffset;
-}
-
-/**
- * @param {Line} newLine
- * @param {Line} originalLine
- */
-function resetSword(newLine, originalLine) {
-    newLine.x1 = originalLine.x1;
-    newLine.y1 = originalLine.y1;
-    newLine.x2 = originalLine.x2;
-    newLine.y2 = originalLine.y2;
-    newLine.color = originalLine.color;
-    newLine.lineWidth = originalLine.lineWidth;
 }
 
 /**
  * @param {Number} elapsed
+ * @param {Array} drawables Reference array to be filled with updated drawables
+ * @param {Array} originals Array containing original entities with drawables (T-pose)
  */
-function updateGame(elapsed) {
+function updateGame(elapsed, drawables, originals) {
+    // State machine stuff
     stateManager.update(elapsed);
     if (mouseDown && !isAttacking) {
         isAttacking = true;
@@ -115,20 +48,22 @@ function updateGame(elapsed) {
         isAttacking = false;
     }
 
-    // playerPosition.x = xMouse * scale;
-    // playerPosition.y = yMouse * scale;
-
     const curState = stateManager.getEntityState(entity);
     if ((1 <= curState.progress) && (false === curState.canLoop)) {
         stateManager.setEntityState(entity, idleingState);
     }
 
-    const currentState = stateManager.getEntityState(entity);
-    const currentAnimations = animations[currentState.name];
-    resetSword(animatedSword, sword);
-    updateSword3(animatedSword, currentAnimations, currentState.progress);
+    for (const original of originals) {
+        addPlayer(drawables, original, elapsed);
+    }
+
 }
 
+/**
+ * @param {Line} bone
+ * @param {Line} sword
+ * @param {Number} pos
+ */
 function adjustHandToSword(bone, sword, pos) {
     // create vector of sword itself
     const swordVector = Vector.subtract(new Vector(sword.x2, sword.y2), new Vector(sword.x1, sword.y1));
@@ -141,190 +76,62 @@ function adjustHandToSword(bone, sword, pos) {
     bone.y2 = sword.y1 + swordVector.y;
 }
 
-function addPlayer(arr, sword) {
-    const bones = {
-        "neck": {
-            "x1": 0,
-            "y1": -40.9,
-            "x2": 0,
-            "y2": -70.9,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "headRect": {
-            "x": -8,
-            "y": -96.9,
-            "width": 16,
-            "height": 20,
-            "color": "red"
-        },
-        "head": {
-            "x1": 0,
-            "y1": -70.9,
-            "x2": 0,
-            "y2": -86.9,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "leftHip": {
-            "x1": 0,
-            "y1": -40.9,
-            "x2": 8,
-            "y2": -42.9,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "leftKnee": {
-            "x1": 8,
-            "y1": -42.9,
-            "x2": 27.6868279518407,
-            "y2": -27.491275030210204,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "leftFoot": {
-            "x1": 27.6868279518407,
-            "y1": -27.491275030210204,
-            "x2": 33,
-            "y2": 0,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "rightHip": {
-            "x1": 0,
-            "y1": -40.9,
-            "x2": -8,
-            "y2": -42.9,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "rightKnee": {
-            "x1": -8,
-            "y1": -42.9,
-            "x2": -11.700864156165679,
-            "y2": -18.17544531245086,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "rightFoot": {
-            "x1": -11.700864156165679,
-            "y1": -18.17544531245086,
-            "x2": -33,
-            "y2": 0,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "leftShoulder": {
-            "x1": 0,
-            "y1": -70.9,
-            "x2": 8,
-            "y2": -70.9,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "leftElbow": {
-            "x1": 8,
-            "y1": -70.9,
-            "x2": 7.074582329637769,
-            "y2": -52.923804570060405,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "leftHand": {
-            "x1": 7.074582329637769,
-            "y1": -52.923804570060405,
-            "x2": 23,
-            "y2": -40.825,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "rightShoulder": {
-            "x1": 0,
-            "y1": -70.9,
-            "x2": -8,
-            "y2": -70.9,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "rightElbow": {
-            "x1": -8,
-            "y1": -70.9,
-            "x2": -21.459169643852487,
-            "y2": -58.94798123754821,
-            "color": "red",
-            "lineWidth": 1
-        },
-        "rightHand": {
-            "x1": -21.459169643852487,
-            "y1": -58.94798123754821,
-            "x2": -13,
-            "y2": -40.825,
-            "color": "red",
-            "lineWidth": 1
-        }
-    }
-    const localBones = {};
-    for (const boneName in bones) {
-        const bone = bones[boneName];
-        if (bone.lineWidth) {
-            localBones[boneName] = new Line(
-                playerPosition.x + Math.floor(bone.x1 / 4),
-                playerPosition.y + Math.floor(bone.y1 / 4),
-                playerPosition.x + Math.floor(bone.x2 / 4),
-                playerPosition.y + Math.floor(bone.y2 / 4),
-                bone.color,
-                bone.lineWidth
-            );
-        } else {
-            localBones[boneName] = new Rectangle(
-                playerPosition.x + Math.floor(bone.x / 4),
-                playerPosition.y + Math.floor(bone.y / 4),
-                Math.floor(bone.width / 4),
-                Math.floor(bone.height / 4),
-                bone.color
-            );
-        }
-    }
+/**
+ * @param {Line} hand
+ * @param {Line} elbow
+ */
+function adjustElbowIK(hand, elbow) {
+    const elbowVector = new Vector(0, 0);
+    const length1 = 20;
+    const length2 = 20;
+    const handVector = new Vector(
+        hand.x2,
+        hand.y2
+    );
+    const shoulderVector = new Vector(
+        elbow.x1,
+        elbow.y1
+    );
+    IKSolver.global(elbowVector, length1, length2, handVector, shoulderVector, 1);
+    elbow.x2 = elbowVector.x;
+    elbow.y2 = elbowVector.y;
+    hand.x1 = elbowVector.x;
+    hand.y1 = elbowVector.y;
+}
 
-    adjustHandToSword(localBones["rightHand"], sword, 0.1);
-    adjustHandToSword(localBones["leftHand"], sword, 0);
+function addPlayer(arr, original, elapsed) {
+    const currentState = stateManager.getEntityState(entity);
+    const currentAnimations = animations[currentState.name];
 
-    const leftElbowVector = new Vector(0, 0);
-    const length1 = 4;
-    const length2 = 5;
-    const leftHandVector = new Vector(
-        localBones["leftHand"].x2,
-        localBones["leftHand"].y2
-    );
-    const leftShoulderVector = new Vector(
-        localBones["leftElbow"].x1,
-        localBones["leftElbow"].y1
-    );
-    IKSolver.global(leftElbowVector, length1, length2, leftHandVector, leftShoulderVector, 1);
-    localBones["leftElbow"].x2 = leftElbowVector.x;
-    localBones["leftElbow"].y2 = leftElbowVector.y;
-    localBones["leftHand"].x1 = leftElbowVector.x;
-    localBones["leftHand"].y1 = leftElbowVector.y;
+    updateBones(original, localBones, currentAnimations, currentState.progress, elapsed);
 
-    const rightElbowVector = new Vector(0, 0);
-    const rightHandVector = new Vector(
-        localBones["rightHand"].x2,
-        localBones["rightHand"].y2
-    );
-    const rightShoulderVector = new Vector(
-        localBones["rightElbow"].x1,
-        localBones["rightElbow"].y1
-    );
-    IKSolver.global(rightElbowVector, length1, length2, rightHandVector, rightShoulderVector, 1);
-    localBones["rightElbow"].x2 = rightElbowVector.x;
-    localBones["rightElbow"].y2 = rightElbowVector.y;
-    localBones["rightHand"].x1 = rightElbowVector.x;
-    localBones["rightHand"].y1 = rightElbowVector.y;
+    adjustHandToSword(localBones.rightHand, localBones.sword, 0.1);
+    adjustHandToSword(localBones.leftHand, localBones.sword, 0);
+    adjustElbowIK(localBones.leftHand, localBones.leftElbow);
+    adjustElbowIK(localBones.rightHand, localBones.rightElbow);
 
     for (const boneName in localBones) {
-        arr.push(localBones[boneName]);
-    }
+        const bone = localBones[boneName];
+        if (Utils.isInstanceOf(Line, bone)) {
+            arr.push(new Line(
+                bone.x1 + playerPosition.x,
+                bone.y1 + playerPosition.y,
+                bone.x2 + playerPosition.x,
+                bone.y2 + playerPosition.y,
+                bone.color,
+                bone.lineWidth));
+        }
 
+        if (Utils.isInstanceOf(Rectangle, bone)) {
+            arr.push(new Rectangle(
+                bone.x + playerPosition.x,
+                bone.y + playerPosition.y,
+                bone.width,
+                bone.height,
+                bone.color
+            ));
+        }
+    }
 }
 
 /**
@@ -332,11 +139,13 @@ function addPlayer(arr, sword) {
  */
 function animationLoop(elapsed) {
     const elapsedSeconds = elapsed * 0.001;
-    updateGame(elapsedSeconds);
+
+    // playerPosition.x = xMouse * scale;
+    // playerPosition.y = yMouse * scale;
 
     const drawables = [];
-    addPlayer(drawables, animatedSword);
-    drawables.push(animatedSword);
+    const originals = [stickFigure];
+    updateGame(elapsedSeconds, drawables, originals);
 
     if (gl) {
         Renderer2d.render(gl, camPos, drawables);
@@ -351,27 +160,58 @@ const stickFigure = {
     "leftKnee": new Line(2, -11, 6, -7, "red", 1),
     "leftFoot": new Line(6, -7, 8, 0, "red", 1),
     "rightHip": new Line(0, -11, -2, -11, "red", 1),
-    "rightKnee": new Line(2, -11, -3, -5, "red", 1),
-    "rightFoot": new Line(3, -5, -9, 0, "red", 1),
+    "rightKnee": new Line(-2, -11, -3, -5, "red", 1),
+    "rightFoot": new Line(-3, -5, -9, 0, "red", 1),
     "leftShoulder": new Line(0, -18, 2, -18, "red", 1),
     "leftElbow": new Line(2, -18, 1, -14, "red", 1),
     "leftHand": new Line(1, -14, 5, -11, "red", 1),
     "rightShoulder": new Line(0, -18, -2, -18, "red", 1),
-    "rightElbow": new Line(2, -18, -6, -15, "red", 1),
-    "rightHand": new Line(6, -15, -4, -11, "red", 1),
+    "rightElbow": new Line(-2, -18, -6, -15, "red", 1),
+    "rightHand": new Line(-6, -15, -4, -11, "red", 1),
     "sword": new Line(8, -16, 8, 0, "red", 1),
 };
 
+for (const boneName in stickFigure) {
+    const bone = stickFigure[boneName];
+    if (Utils.isInstanceOf(Line, bone)) {
+        bone.x1 *= 4;
+        bone.y1 *= 4;
+        bone.x2 *= 4;
+        bone.y2 *= 4;
+        bone.lineWidth *= 4;
+    }
+
+    if (Utils.isInstanceOf(Rectangle, bone)) {
+        bone.x *= 4;
+        bone.y *= 4;
+        bone.width *= 4;
+        bone.height *= 4;
+    }
+}
+
+
+const localBones = {};
+for (const boneName in stickFigure) {
+    const bone = stickFigure[boneName];
+    if (Utils.isInstanceOf(Line, bone)) {
+        localBones[boneName] = new Line(bone.x1, bone.y1, bone.x2, bone.y2, bone.color, bone.lineWidth);
+    }
+
+    if (Utils.isInstanceOf(Rectangle, bone)) {
+        localBones[boneName] = new Rectangle(bone.x, bone.y, bone.width, bone.height, bone.color);
+    }
+}
+
 const content = document.createElement("div");
 const canvas = document.createElement('canvas');
-const scale = 1 / 4;
+const scale = 1;
 const canvasWidth = 800 * scale;
 const canvasHeight = 600 * scale;
+// Consider 640 x 360 or 320 x 180?
 const gl = canvas.getContext("2d");
 
-const playerPosition = new Vector(60, 82);
+const playerPosition = new Vector(200, 200);
 const camPos = new Vector(0, 0);
-const animatedSword = new Line(0, 0, 0, 0, "white");
 
 const stateManager = new StateManager();
 const entity = new Entity("foo");
@@ -379,39 +219,90 @@ const swinging = new Intent("swinging");
 const swingingState = new State('swinging', 0.5);
 const swingingBackState = new State('swingingBack', 0.3);
 const jabForwardState = new State('jabForward', 0.3);
-const idleingState = new State('idleing', 1, true);
+const idleingState = new State('idleing', 2, true);
 
 const animations = {
     idleing: [
         {
-            xStart: (t) => -4,
-            yStart: (t) => Lerp.calc(2, 1, Lerp.parabola(t)),
-            xEnd: (t) => -24,
-            yEnd: (t) => Lerp.calc(2, 1, Lerp.parabola(t)),
+            headRect: {
+                y: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+            },
+            neck: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t))
+            },
+            head: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t))
+            },
+            leftHip: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t))
+            },
+            leftKnee: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+            },
+            // leftFoot: {},
+            rightHip: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t))
+            },
+            rightKnee: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+            },
+            // rightFoot: {},
+            leftShoulder: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t))
+            },
+            leftElbow: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+            },
+            // leftHand: {},
+            rightShoulder: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t))
+            },
+            rightElbow: {
+                y1: (t) => Lerp.calc(0.5, 0, Lerp.parabola(t)),
+            },
+            // rightHand: {},
+            sword: {
+                x1: (t) => -4,
+                y1: (t) => Lerp.calc(0, 1, Lerp.parabola(t)),
+                x2: (t) => -24,
+                y2: (t) => Lerp.calc(0, 1, Lerp.parabola(t)),
+            },
         },
     ],
     swinging: [
         {
-            xStart: (t) => Lerp.calc(-6, 0, Lerp.sin(t)),
-            yStart: (t) => Lerp.calc(-6, 0, Lerp.linear(t)),
-            xEnd: (t) => Lerp.calc(-24, 31, Lerp.sin(t)),
-            yEnd: (t) => Lerp.calc(0, -41, Lerp.linear(t)),
+            sword: {
+                x1: (t) => Lerp.calc(-6, 0, Lerp.sin(t)),
+                y1: (t) => Lerp.calc(-6, 0, Lerp.linear(t)),
+                x2: (t) => Lerp.calc(-24, 31, Lerp.sin(t)),
+                y2: (t) => Lerp.calc(0, -41, Lerp.linear(t)),
+            }
         },
     ],
     swingingBack: [
         {
-            xStart: (t) => Lerp.calc(-6, 0, Lerp.sin(t)),
-            yStart: (t) => Lerp.calc(-6, 0, Lerp.linear(t)),
-            xEnd: (t) => Lerp.calc(-30, 37, Lerp.sin(t)),
-            yEnd: (t) => Lerp.calc(-45, 0, Lerp.linear(t)),
+            sword: {
+                x1: (t) => Lerp.calc(-6, 0, Lerp.sin(t)),
+                y1: (t) => Lerp.calc(-6, 0, Lerp.linear(t)),
+                x2: (t) => Lerp.calc(-30, 37, Lerp.sin(t)),
+                y2: (t) => Lerp.calc(-45, 0, Lerp.linear(t)),
+            }
         }
     ],
     jabForward: [
         {
-            xStart: (t) => Lerp.calc(-12, 18, Lerp.parabola(t)),
-            yStart: (t) => Lerp.calc(-6, 0, Lerp.linear(t)),
-            xEnd: (t) => Lerp.calc(13, 43, Lerp.parabola(t)),
-            yEnd: (t) => Lerp.calc(-23, -17, Lerp.linear(t)),
+            sword: {
+                x1: (t) => Lerp.calc(-12, 18, Lerp.parabola(t)),
+                y1: (t) => Lerp.calc(-6, 0, Lerp.linear(t)),
+                x2: (t) => Lerp.calc(13, 43, Lerp.parabola(t)),
+                y2: (t) => Lerp.calc(-23, -17, Lerp.linear(t)),
+            }
         }
     ]
 };
